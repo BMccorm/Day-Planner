814eefef89f0a9699e6f6c1253ce8db4f4835f7d
Brielle McCormick <briellemccormick@Brielles-MBP.fios-router.home>
2020-07-11 15:16:22 -0400

    saved input value to local storage) git push q

--- script.js
+++ script.js
@@ -5,26 +5,28 @@ console.log(moment().format("MMMM Do YYYY, h:mm a"));
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
 console.log(currentTime);
 
-var timeArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+const timeArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
+//2. creating one input area with btn, then run for loop to dynamically add the rest
 for (var i = 0; i < timeArray.length; i++) {
   var span = $("<span>").attr("class", "input-group-text");
   var d1 = $("<div>").attr("class", "input-group mb-3");
   var d2 = $("<div>").attr("class", "input-group-prepend");
-  var input = $("<input>").attr({
-    class: "form-control userInput",
-    type: "text",
-  });
-  var btn = $("<button>").attr({
-    class: "btn btn-outline-secondary submit",
-    type: "button",
-  });
+  var input = $("<input>")
+    .attr("class", "form-control userInput")
+    .attr("id", "input" + i);
+
+  var dataIndex = input.attr("data-index", i);
+  console.log(dataIndex);
+
+  var btnEl = $("<button>").attr("class", "btn btn-outline-secondary submit");
+  // btn.textContent = "test";
 
   $(".container").append(d1);
   d1.append(d2);
   d2.append(span);
   d1.append(input);
-  d1.append(btn);
+  d1.append(btnEl);
 
   if (timeArray[i] == 12) {
     span.text(timeArray[i] + " PM");
@@ -41,16 +43,21 @@ for (var i = 0; i < timeArray.length; i++) {
   } else if (currentTime - timeArray[i] > 0) {
     span.attr("class", "past");
   }
-  console.log(timeArray[i]);
-}
 
-//2. creating one input area with btn, then run for to dynamically add the rest
+  btnEl.on("click", function (event) {
+    var element = event.target;
+    if (element.matches("button")) {
+      let value = $(this).prev().val();
+      var dataIndex = $(this).prev().attr("data-index");
+      console.log($(this).prev().val());
+      localStorage.setItem(dataIndex, value);
+    }
+  });
+}
 
 //3. local storage
 //store data into localStorage
 //get the data from localStorage then append to specific input area
-//hour9= "Test1"
-//key="9" value="Test1"
 
 // <div 1 class="input-group mb-3">
 //   <div 2 class="input-group-prepend">
